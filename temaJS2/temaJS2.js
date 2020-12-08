89edd3a8e79126608bb050c3b4cc8555e1b7663c
Cristian-Pavel <69677844+Cristian-Pavel@users.noreply.github.com>
2020-12-08 11:44:01 +0200

    problem 7 solved

--- temaJS2/temaJS2.js
+++ temaJS2/temaJS2.js
@@ -102,3 +102,13 @@ function invertAllStrings(list) {
   }
   return invers;
 }
+
+// 7. Calculeaza factorialul unui numar ("factorial")
+
+function factorial(n) {
+  let f = 1;
+  for (let i = 1; i <= n; i++) {
+    f *= i;
+  }
+  return f;
+}
