1c45cec40cb2944fc3f8ced536f614ca655c8d91
Cristian-Pavel <69677844+Cristian-Pavel@users.noreply.github.com>
2020-12-11 12:04:11 +0200

    partially functional (page load automatically)

--- temaDOM-manipulation1/validate.js
+++ temaDOM-manipulation1/validate.js
@@ -11,5 +11,22 @@ function validateCompletion() {
     elements.lastName.classList.add("empty");
   } else if (elements.textArea.value === "") {
     elements.textArea.classList.add("empty");
+  } else {
+    elements.textArea.classList.remove("empty");
   }
 }
+
+// function draw() {
+//   let str = "";
+//   for (let i = messages2.length - 1; i >= 0; i--) {
+//     str += `
+// 					<div class="message">
+// 						<div class="user">${messages2[i].user}</div>
+// 						<div class="content">${messages2[i].message}</div>
+// 					</div>
+// 				`;
+//   }
+
+//   let chatMessages = document.querySelector("#chatMessages");
+//   chatMessages.innerHTML = str;
+// }
