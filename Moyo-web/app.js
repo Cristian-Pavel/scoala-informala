6ca1ee9a15d6463dd8ead8508ea1c74c8d8a840e
Cristian-Pavel <69677844+Cristian-Pavel@users.noreply.github.com>
2021-02-15 19:59:37 +0200

    add activity html and weekview drafts

--- Moyo-web/app.js
+++ Moyo-web/app.js
@@ -142,6 +142,31 @@ function drawTable() {
 
 // Week view page
 
+function drawCalendar(event) {
+  let calendarEl = document.getElementById("calendar");
+  let calendar = new FullCalendar.Calendar(calendarEl, {
+    initialView: "dayGridWeek",
+    nowIndicator: true,
+    // initialDate: "2021-02-07",
+    headerToolbar: {
+      left: "prev,next,today",
+      center: "title",
+      right: "dayGridMonth,timeGridWeek,timeGridDay",
+    },
+    events: [
+      {
+        groupId: "productive",
+        title: "Scris cod",
+        start: "2021-02-15T16:00:00",
+        end: "2021-02-15T20:00:00",
+        backgroundColor: "green",
+      },
+    ],
+  });
+
+  calendar.render();
+}
+
 // Statisics page
 
 ///////////////////////
@@ -154,22 +179,36 @@ selectDOM.burgerMenuBtn.addEventListener("click", toggleMobileMenu);
 // How to page
 
 // Set goals page
-
-selectDOM.addNewGoalBtn.addEventListener("click", addNewGoal);
-window.addEventListener("load", getDataFromDataBase);
-selectDOM.cancelBtnEdit.addEventListener("click", cancel);
-selectDOM.confirmBtnEdit.addEventListener("click", confirmEditOfGoal);
+if (window.location.href === "http://127.0.0.1:5500/setGoals.html") {
+  selectDOM.addNewGoalBtn.addEventListener("click", addNewGoal);
+  window.addEventListener("load", getDataFromDataBase);
+  selectDOM.cancelBtnEdit.addEventListener("click", cancel);
+  selectDOM.confirmBtnEdit.addEventListener("click", confirmEditOfGoal);
+}
 
 // Add activity page
 
 // Week view page
 
+if (window.location.href === "http://127.0.0.1:5500/weekView.html") {
+  window.addEventListener("load", drawCalendar);
+}
+
 // Statisics page
 
 /**
- * //////////////// Set goals page
+ * //////////////// weekView
  * TODO:
-  
- 
+  1) Adaugarea de evenimente se face prin adaugarea unui obiect in array-ul events (click aici, minutul 6:25 - https://www.youtube.com/watch?v=hyVzloriEfo) 
+  2) Evenimentele se adauga sub forma unui array de obiecte (dupa cum arata in codul de aici de la linia 59 view-source:https://fullcalendar.io/docs/initialize-globals-demo)
+  3) Eu as putea adauga ca proprietati la fiecare obiect: 
+      3.1) Tipul activitatii (productiva, de relaxare, mentenanta, distrageri) 
+      3.2) Ora incepere 
+      3.3) Ora finalizare
+      3.4) Descriere
+  4) De pe pagina add new pot pune o functie (formular cu date picker/hour selector) unde va fi o functie care va trimite un obiect
 
+
+  CSS-ul la weekview: 
+  1) Colorarea calendarului pe categorii: https://fullcalendar.io/docs/event-object - backgroundColor: "green" - proprietate in eveniment
  */
